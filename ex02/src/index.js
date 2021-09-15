ffd12d5974bc4eb10c649cba043b80daff1bd892
nedimmehuljic <nedim.mehuljic>
2021-09-16 01:48:51 +0200

    Repo03

---
+++ ex02/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+ //Only change code below this line
+
+ /*document.write("This multi-line comment");
+document.write("This multi-line comment");
+document.write("This multi-line comment");
+document.write("This multi-line comment");
+document.write("This multi-line comment");
+*/
+
+ //Only change code above this line
+
+}
+
+ console.log(main());
+
+ module.exports = main;
